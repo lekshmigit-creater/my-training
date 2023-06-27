6742dc23ad52ee3c28e931a593fc2a3453af4b1f
Lekshmi.S.Nair <136094821+lekshmigit-creater@users.noreply.github.com>
2023-06-27 18:59:47 +0530

    Add excel read files to git hub

---
+++ Excelread.java
@@ -0,0 +1,29 @@
+package ExcelRead;
+import java.io.FileInputStream;
+import java.io.IOException;
+
+import org.apache.poi.hssf.usermodel.HSSFCell;
+import org.apache.poi.hssf.usermodel.HSSFRow;
+import org.apache.poi.hssf.usermodel.HSSFSheet;
+import org.apache.poi.hssf.usermodel.HSSFWorkbook;
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.ss.usermodel.Cell;
+public class Excelread {
+	public static void main(String args[]) throws IOException
+	{
+		FileInputStream file= new FileInputStream(path.Test_PATH);
+	HSSFWorkbook workbook=new HSSFWorkbook(file);
+	HSSFSheet sheet= workbook.getSheetAt(0);
+	HSSFRow row=sheet.getRow(0);
+	HSSFCell cell=row.getCell(1);
+	System.out.println(cell);
+	
+for(Row r:sheet)
+{
+	for(Cell c:r) {
+		System.out.println(c+"");
+	}
+	System.out.println();
+}
+	}
+}
---
+++ path.java
@@ -0,0 +1,6 @@
+package ExcelRead;
+
+public class path {
+	public static final String Test_PATH="C:\\Users\\user\\Desktop\\Test.xls";
+
+}
